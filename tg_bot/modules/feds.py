8e849f11d7fa26a6a78cda6211db28bd7b1c1ad5
Rowland <edwinmarita12@gmail.com>
2019-05-12 12:57:53 +0300

    Update feds.py

--- tg_bot/modules/feds.py
+++ tg_bot/modules/feds.py
@@ -510,7 +510,7 @@ __help__ = """
 Ah, group management. It's all fun and games, until you start getting spammers in, and you need to ban them. Then you need to start banning more, and more, and it gets painful.
 But then you have multiple groups, and you don't want these spammers in any of your groups - how can you deal? Do you have to ban them manually, in all your groups?
 
-Inspired by <b>Miss Rose Bot</b>
+Inspired by Miss Rose Bot
 
 No more! With federations, you can make a ban in one chat overlap to all your other chats.
 You can even appoint federation admins, so that your trustworthy admins can ban across all the chats that you want to protect.
